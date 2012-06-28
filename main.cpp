f3366ecb6c1d5ffd7ede72764f9e8035c352ab43
Thomas Yeung <y.tom.acct@gmail.com>
2012-06-28 10:40:38 -0700

    more comments
    added more comments

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ typedef unsigned long long val;
 typedef val Denomination;
 
 /*
-  self memory releasing array
+  self releasing array
 */
 struct valptr
 {
@@ -48,10 +48,11 @@ public:
 */
 inline val rest_of_five (val restfive)
 {
-    val restfive5 = restfive/5;
-    val restfive10 = restfive5/2;
-    val maxone = restfive10/10;
+    val restfive5 = restfive/5; // the number of leftover pennies divided by 5
+    val restfive10 = restfive/10; // the number of leftover pennies divided by 10
+    val maxone = restfive/100; // maximum number of one dollar bills given the total pennies
 
+    // closed expression solution...
     return ((100*maxone - 15*(restfive5) + 20)*maxone
                 + 3*(restfive5)*((restfive10) + 1)
                 - 3*(restfive10)*(restfive10)
@@ -70,9 +71,10 @@ val find_solution (val total_pennies)
     val total_nickels = total_pennies/5;
     solution_table solution (total_nickels);
 
-    static val coin_value[6] = {500,1000,2000,5000,10000,25};
-    static val coin_value5[6] = {100,200,400,1000,2000,5};
+    static const val coin_value[6] = {500,1000,2000,5000,10000,25};
+    static const val coin_value5[6] = {100,200,400,1000,2000,5}; // coin_value divided by 5
 
+    // first of six loops. generates all the solutions for the DP stage five dollars
     for (val nickels=0; nickels<solution.array_size(); nickels++)
     {
         val pennies = 5*nickels;
@@ -81,11 +83,16 @@ val find_solution (val total_pennies)
         else
             solution (0,nickels) = rest_of_five(pennies);
     }
+
+    // rest of the six loops.
     for (Denomination d=1; d<=5; d++)
     {
-        val d1 = d % 2;
-        val d2 = (d-1) % 2;
+        val d1 = d % 2; // index of current denomination
+        val d2 = (d-1) % 2; // index of previous denomination
+
+        // the end of the following loop
         val end = ( coin_value5[d]<solution.array_size() ? coin_value5[d] : solution.array_size() );
+
         for (val nickels=0; nickels<end; nickels++)
             solution (d1,nickels) = solution (d2,nickels);
         for (val nickels=coin_value5[d]; nickels<solution.array_size(); nickels++)
